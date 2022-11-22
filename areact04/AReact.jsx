2b20e16cce2999ad500ae3a80fde3a52982e6f96
Cam Song <neosoyn@gmail.com>
2022-11-22 20:41:46 +0800

    copy areact03 to areact04

---
+++ areact04/AReact.jsx
@@ -0,0 +1,135 @@
+import '../requestIdleCallbackPolyfill';
+
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => {
+        return typeof child !== 'object' ? createTextElement(child) : child;
+      }),
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: 'HostText',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+const isProperty = (key) => key !== 'children';
+let workInProgress = null;
+let workInProgressRoot = null;
+
+class AReactDomRoot {
+  _internalRoot = null;
+  constructor(container) {
+    this._internalRoot = {
+      current: null,
+      containerInfo: container,
+    };
+  }
+
+  render(element) {
+    this._internalRoot.current = {
+      alternate: {
+        stateNode: this._internalRoot.containerInfo,
+        props: {
+          children: [element],
+        },
+      },
+    };
+    workInProgressRoot = this._internalRoot;
+    workInProgress = workInProgressRoot.current.alternate;
+    window.requestIdleCallback(workloop);
+
+    // this.renderImpl(element, this.container);
+  }
+}
+
+function workloop() {
+  while (workInProgress) {
+    workInProgress = performUnitOfWork(workInProgress);
+  }
+}
+
+function performUnitOfWork(fiber) {
+  // 处理当前 fiber：创建 DOM，设置 props，插入当前 dom 到 parent
+  if (!fiber.stateNode) {
+    fiber.stateNode =
+      fiber.type === 'HostText'
+        ? document.createTextNode('')
+        : document.createElement(fiber.type);
+    Object.keys(fiber.props)
+      .filter(isProperty)
+      .forEach((key) => {
+        fiber.stateNode[key] = fiber.props[key];
+      });
+  }
+  if (fiber.return) {
+    fiber.return.stateNode.appendChild(fiber.stateNode);
+  }
+  // 初始化 children 的 fiber
+  let prevSibling = null;
+  fiber.props.children.forEach((child, index) => {
+    const newFiber = {
+      type: child.type,
+      stateNode: null,
+      props: child.props,
+      return: fiber,
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+  });
+
+  // 返回下一个处理的 fiber
+  return getNextFiber(fiber);
+}
+
+function getNextFiber(fiber) {
+  //   遍历顺序：
+  // 先遍历 child
+  // 然后是 sibling
+  // 然后是 return 并找下一个 sibling
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    } else {
+      nextFiber = nextFiber.return;
+    }
+  }
+  return null;
+}
+
+function createRoot(container) {
+  return new AReactDomRoot(container);
+}
+
+function act(callback) {
+  callback();
+  return new Promise((resolve) => {
+    function loop() {
+      if (workInProgress) {
+        window.requestIdleCallback(loop);
+      } else {
+        resolve();
+      }
+    }
+    loop();
+  });
+}
+
+export default { createElement, createRoot, act };
---
+++ areact04/jsx.test.jsx
@@ -0,0 +1,102 @@
+import { describe, it, expect } from 'vitest';
+import AReact from './AReact';
+const act = AReact.act;
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+describe('AReact JSX', () => {
+  it('should render jsx', async () => {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo">
+        <div id="bar"></div>
+        <button></button>
+      </div>
+    );
+    console.log(element);
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(element);
+    });
+    expect(container.innerHTML).toBe(
+      '<div id="foo"><div id="bar"></div><button></button></div>'
+    );
+  });
+
+  it('should render jsx with text', async () => {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo">
+        <div id="bar">hello</div>
+        <button>Add</button>
+      </div>
+    );
+    console.log(JSON.stringify(element));
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(element);
+    });
+    expect(container.innerHTML).toBe(
+      '<div id="foo"><div id="bar">hello</div><button>Add</button></div>'
+    );
+  });
+
+  it('should render jsx with different props', async () => {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo" className="bar">
+        <button></button>
+      </div>
+    );
+    console.log(element);
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(element);
+    });
+    expect(container.innerHTML).toBe(
+      '<div id="foo" class="bar"><button></button></div>'
+    );
+  });
+});
+
+describe('AReact Concurrent', () => {
+  it('should render in async', async () => {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo">
+        <div id="bar"></div>
+        <button></button>
+      </div>
+    );
+    console.log(element);
+    const root = AReact.createRoot(container);
+    root.render(element);
+    expect(container.innerHTML).toBe('');
+    await sleep(1000);
+    expect(container.innerHTML).toBe(
+      '<div id="foo"><div id="bar"></div><button></button></div>'
+    );
+  });
+
+  it('should render in async', async () => {
+    const container = document.createElement('div');
+    const element = (
+      <div id="foo">
+        <div id="bar"></div>
+        <button></button>
+      </div>
+    );
+    console.log(element);
+    const root = AReact.createRoot(container);
+    await act(() => {
+      root.render(element);
+      expect(container.innerHTML).toBe('');
+    });
+
+    expect(container.innerHTML).toBe(
+      '<div id="foo"><div id="bar"></div><button></button></div>'
+    );
+  });
+});
